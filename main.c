1874c22047a1f359fcf655b4673712ede404a576
Rishu Agrawal <rishu.agrawal@ucdconnect.ie>
2023-03-30 15:48:58 +0000

    Adding display menu function.

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 
+//Function Definitions
+void displayMenu();
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+   displayMenu();
+}
+
+void displayMenu(){
+
+    int choice;
+    printf("Menu:\n ");
+    printf("1. Display board:\n ");
+    printf("2. Load board from a file:\n ");
+    printf("3. Edit list: \n ");
+    printf("4. Edit Board: \n ");
+    printf("5. Save board to a file: \n ");
+    printf("6. Quit: \n ");
+
+
+
+    int flag=0;
+    do{
+        if(flag==0){
+            printf("Enter your choice: \n");
+            scanf("%d", &choice);
+        }
+        else{
+            printf("Invalid Input. Please try again: \n");
+            printf("Enter your choice: \n");
+            scanf("%d", &choice);
+        }
+        flag++;
+    }
+    while(choice>6||choice<1);
+
+
 }
